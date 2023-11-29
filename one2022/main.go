11361ee7479ad0377a6c25d853b301e95601ab37
Derek Mulvihill <mulvihd@gmail.com>
2023-11-29 14:35:07 -0600

    feat: add 2022 problem one

---
+++ one2022/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	log.SetFlags(0)
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	maxCalories := [3]int{0, 0, 0}
+	calories := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			for i := 0; i < len(maxCalories); i++ {
+				if calories > maxCalories[i] {
+					for j := len(maxCalories) - 1; j > i; j-- {
+						maxCalories[j] = maxCalories[j-1]
+					}
+					maxCalories[i] = calories
+					break
+				}
+			}
+			calories = 0
+		} else {
+			lcal, err := strconv.Atoi(line)
+			if err != nil {
+				log.Panic(err)
+			}
+			calories += lcal
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("The most calories that a single elf is carrying is %d.", maxCalories[0])
+	log.Printf("The most calories that the the top 3 elves are carrying is %d.", maxCalories[0]+maxCalories[1]+maxCalories[2])
+}
