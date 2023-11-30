6f4d2fe5bf7a16b3e767648bf3a41a59c527ef05
Derek Mulvihill <mulvihd@gmail.com>
2023-11-30 15:55:41 -0600

    feat: add 2022 problem four

---
+++ 2022/04/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	log.SetFlags(0)
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	containedCount := 0
+	overlapCount := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ",")
+		nums1 := parseNums(parts[0])
+		nums2 := parseNums(parts[1])
+		if nums1[0] <= nums2[0] && nums1[1] >= nums2[1] {
+			containedCount++
+		} else if nums2[0] <= nums1[0] && nums2[1] >= nums1[1] {
+			containedCount++
+		}
+		if nums1[0] <= nums2[1] && nums1[1] >= nums2[0] {
+			overlapCount++
+		}
+	}
+	log.Printf("The number of pairs with a range fully containing the other is %d.", containedCount)
+	log.Printf("The number of pairs with an overlap is %d.", overlapCount)
+}
+
+func parseNums(s string) [2]int {
+	parts := strings.Split(s, "-")
+	nums := [2]int{}
+	for i := 0; i < len(nums); i++ {
+		v, err := strconv.Atoi(parts[i])
+		if err != nil {
+			log.Panic(err)
+		}
+		nums[i] = v
+	}
+	return nums
+}
