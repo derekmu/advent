95b953481e2ad039bc6fa864ba429d3ec2bd5e49
Derek Mulvihill <mulvihd@gmail.com>
2023-12-03 22:19:22 -0600

    feat: add 2022 problem ten

---
+++ 2022/day10/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"advent/util"
+	"bytes"
+	"log"
+	"time"
+)
+
+type op struct {
+	name  string
+	value int
+}
+
+func main() {
+	input := util.ReadInput()
+
+	start := time.Now()
+
+	lines := util.ParseInputLines(input)
+	ops := make([]op, 0, len(lines))
+	for _, line := range lines {
+		name, valueStr, _ := bytes.Cut(line, []byte(" "))
+		value := 0
+		if bytes.Equal(name, []byte("addx")) {
+			value = util.Btoi(valueStr)
+		}
+		ops = append(ops, op{
+			name:  string(name),
+			value: value,
+		})
+	}
+
+	parse := time.Now()
+
+	end := time.Now()
+
+	part1 := 0
+	part2 := -1
+	x := 1
+	cycle := 0
+	screen := [6][40]bool{}
+	nextCycle := func() {
+		cycle++
+		if cycle >= 20 && cycle <= 220 && (cycle-20)%40 == 0 {
+			part1 += cycle * x
+		}
+		row := (cycle - 1) / 40
+		col := (cycle - 1) % 40
+		screen[row][col] = x >= col-1 && x <= col+1
+	}
+	for _, o := range ops {
+		switch o.name {
+		case "noop":
+			nextCycle()
+		case "addx":
+			nextCycle()
+			nextCycle()
+			x += o.value
+		default:
+			log.Panicf("unknown operation %s", o.name)
+		}
+	}
+
+	for _, row := range screen {
+		for i := 0; i < len(row); i++ {
+			if row[i] {
+				print("#")
+			} else {
+				print(" ")
+			}
+		}
+		println()
+	}
+
+	util.PrintResults(part1, part2, start, parse, end)
+}
--- util/parse.go
+++ util/parse.go
@@ -6,11 +6,18 @@ import (
 
 func Btoi(bytes []byte) int {
 	var result int
+	negate := bytes[0] == '-'
+	if negate {
+		bytes = bytes[1:]
+	}
 	for _, b := range bytes {
 		if b < '0' || b > '9' {
 			log.Panicf("invalid character %d %c", b, b)
 		}
 		result = result*10 + int(b-'0')
+	}
+	if negate {
+		return -result
 	}
 	return result
 }
