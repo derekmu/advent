21d5adafa2adbe22788eb15f7af7aa6256464ff5
Derek Mulvihill <mulvihd@gmail.com>
2023-12-04 11:10:53 -0600

    tweak: update result format

--- util/result.go
+++ util/result.go
@@ -6,6 +6,6 @@ import (
 )
 
 func PrintResults(part1, part2 int, start, parse, end time.Time) {
-	log.Printf("Answers (part 1/part 2):      %d / %d", part1, part2)
-	log.Printf("Timing (parse/execute/total): %v / %v / %v", parse.Sub(start), end.Sub(parse), end.Sub(start))
+	log.Printf("Answers (part 1 / part 2):        %d / %d", part1, part2)
+	log.Printf("Timing (parse + execute = total): %v + %v = %v", parse.Sub(start), end.Sub(parse), end.Sub(start))
 }
