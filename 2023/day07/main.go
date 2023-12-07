fbf3188b0566a19c0fbc58cda8f15fa245eb8720
Derek Mulvihill <mulvihd@gmail.com>
2023-12-07 11:00:01 -0600

    feat: add 2023 problem seven part 2

--- 2023/day07/main.go
+++ 2023/day07/main.go
@@ -31,6 +31,16 @@ var cardMap = map[byte]byte{
 	'2': 0,
 }
 
+const (
+	fiveKind  = 10
+	fourKind  = 9
+	fullHouse = 8
+	threeKind = 7
+	twoPair   = 6
+	onePair   = 5
+	bupkis    = 4
+)
+
 func Run(input []byte) error {
 	start := time.Now()
 
@@ -46,9 +56,9 @@ func Run(input []byte) error {
 			cards[i] = cardMap[cards[i]]
 			counts[cards[i]]++
 			if counts[cards[i]] == 5 {
-				class1 = 10
+				class1 = fiveKind
 			} else if counts[cards[i]] == 4 {
-				class1 = 9
+				class1 = fourKind
 				hasTrio = false
 			} else if counts[cards[i]] == 3 {
 				hasTrio = true
@@ -60,17 +70,50 @@ func Run(input []byte) error {
 		if class1 == 0 {
 			if hasTrio {
 				if duoCount == 1 {
-					class1 = 8
+					class1 = fullHouse
 				} else {
-					class1 = 7
+					class1 = threeKind
 				}
 			} else if duoCount == 2 {
-				class1 = 6
+				class1 = twoPair
 			} else if duoCount == 1 {
-				class1 = 5
+				class1 = onePair
+			} else {
+				class1 = bupkis
 			}
 		}
-		hands = append(hands, &hand{cards: cards, bid: util.Btoi(bid), class1: class1})
+		jokerCount := counts[9]
+		class2 := class1
+		if jokerCount == 4 {
+			class2 = fiveKind
+		} else if jokerCount == 3 {
+			if duoCount == 1 {
+				class2 = fiveKind
+			} else {
+				class2 = fourKind
+			}
+		} else if jokerCount == 2 {
+			if hasTrio {
+				class2 = fiveKind
+			} else if duoCount == 2 {
+				class2 = fourKind
+			} else {
+				class2 = threeKind
+			}
+		} else if jokerCount == 1 {
+			if class2 == fourKind {
+				class2 = fiveKind
+			} else if hasTrio {
+				class2 = fourKind
+			} else if duoCount == 2 {
+				class2 = fullHouse
+			} else if duoCount == 1 {
+				class2 = threeKind
+			} else {
+				class2 = onePair
+			}
+		}
+		hands = append(hands, &hand{cards: cards, bid: util.Btoi(bid), class1: class1, class2: class2})
 	}
 
 	parse := time.Now()
@@ -93,7 +136,30 @@ func Run(input []byte) error {
 		part1 += h.bid * (i + 1)
 	}
 
-	part2 := -1
+	slices.SortFunc(hands, func(a, b *hand) int {
+		c := cmp.Compare(a.class2, b.class2)
+		if c != 0 {
+			return c
+		}
+		for i := 0; i < 5; i++ {
+			if a.cards[i] == 9 && b.cards[i] == 9 {
+				continue
+			} else if a.cards[i] == 9 {
+				return -1
+			} else if b.cards[i] == 9 {
+				return 1
+			}
+			c = cmp.Compare(a.cards[i], b.cards[i])
+			if c != 0 {
+				return c
+			}
+		}
+		return 0
+	})
+	part2 := 0
+	for i, h := range hands {
+		part2 += h.bid * (i + 1)
+	}
 
 	end := time.Now()
 
