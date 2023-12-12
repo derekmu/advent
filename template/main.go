e96aa16675423865a0ba0b2ac4f9cb6bd13f044f
Derek Mulvihill <mulvihd@gmail.com>
2023-12-12 11:27:32 -0600

    feat: add template directory

---
+++ template/main.go
@@ -0,0 +1,37 @@
+package template
+
+import (
+	"advent/util"
+	_ "embed"
+	"time"
+)
+
+//go:embed input.txt
+var Input []byte
+
+func parseInput(input []byte) (lines [][]byte) {
+	lines = util.ParseInputLines(input)
+	return lines
+}
+
+func Run(input []byte) (*util.Result, error) {
+	start := time.Now()
+
+	lines := parseInput(input)
+	_ = lines
+
+	parse := time.Now()
+
+	part1 := -1
+	part2 := -1
+
+	end := time.Now()
+
+	return &util.Result{
+		Part1:     part1,
+		Part2:     part2,
+		StartTime: start,
+		ParseTime: parse,
+		EndTime:   end,
+	}, nil
+}
