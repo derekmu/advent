2fb33e626746ed993deafdf2726b048e4cc7fa74
Derek Mulvihill <mulvihd@gmail.com>
2023-12-20 10:21:46 -0600

    fix: include last line of input when parsing if the file doesn't end with a new line
    i'm annoyed by creating sample inputs without adding a new line at the end

--- util/input.go
+++ util/input.go
@@ -8,15 +8,20 @@ var newline = []byte{'\n'}
 
 func ParseInputLines(input []byte) [][]byte {
 	count := bytes.Count(input, newline)
+	if input[len(input)-1] != '\n' {
+		count++
+	}
 	result := make([][]byte, 0, count)
 	var line []byte
 	var found bool
 	for row := 0; ; row++ {
 		line, input, found = bytes.Cut(input, newline)
+		if found || len(line) > 0 {
+			result = append(result, line)
+		}
 		if !found {
 			break
 		}
-		result = append(result, line)
 	}
 	return result
 }
